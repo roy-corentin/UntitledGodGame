69762661a2890c5b821e6d45c4d834cf9f1ff0d6
Guillaume V <72015697+GuillaumeVernizeau@users.noreply.github.com>
2024-11-10 15:33:23 +0100

    FEAT: SmoothTools

---
+++ Assets/Scripts/Tools/ToolsActions/SmoothHeight.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothHeight : ToolAction
+{
+    [SerializeField] private float smoothingSpeed = 0.5f;
+
+    void Awake()
+    {
+        actionType = PlayerAction.SmoothHeight;
+        toolGO = gameObject;
+    }
+
+    public override void Action(float pressure)
+    {
+        SelectedDots selectedDots = PlayerActions.Instance.GetSelectedDots();
+
+        if (selectedDots.centerDot.dot == null) return;
+        float finalY = selectedDots.centerDot.dot.transform.position.y;
+
+        if (showSelectedDots)
+            selectedDots.centerDot.dot.gameObject.SetActive(true);
+
+        for (int circleIndex = 0; circleIndex < selectedDots.surroundingCircles.Count; circleIndex++)
+        {
+            List<SelectedDot> currentCircle = selectedDots.surroundingCircles[circleIndex];
+            float smoothingFactor = (1.0f - ((float)circleIndex / selectedDots.surroundingCircles.Count)) * smoothingSpeed;
+            
+            foreach (SelectedDot dot in currentCircle)
+            {
+                float currentY = dot.dot.transform.position.y;
+                float smoothedY = Mathf.Lerp(currentY, finalY, smoothingFactor * Time.deltaTime);
+                
+                dot.dot.SetYPosition(smoothedY);
+                if (showSelectedDots)
+                    dot.dot.gameObject.SetActive(true);
+            }
+        }
+
+
+        MapGenerator.Map.Instance.CreateMesh();
+        MapGenerator.Map.Instance.UpdateHeightMap(selectedDots);
+    }
+}
