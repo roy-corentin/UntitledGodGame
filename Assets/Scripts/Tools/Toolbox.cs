c8f742e682f02bd5468b76b5c89bb0a189836957
Arthur <arthur.decaen@epitech.eu>
2024-11-26 17:16:07 +0100

    UP: no more magic number + animal script in script fodler

--- Assets/Scripts/Tools/Toolbox.cs
+++ Assets/Scripts/Tools/Toolbox.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Toolbox : MonoBehaviour
 {
     public Camera playerCamera;
+    [SerializeField] private float distanceForward = 0.35f;
+    [SerializeField] private float distanceUp = -0.35f;
 
     public void Update()
     {
         transform.position = playerCamera.transform.position
-            + playerCamera.transform.forward * 0.35f
-            + playerCamera.transform.up * -0.35f;
+            + playerCamera.transform.forward * distanceForward
+            + playerCamera.transform.up * distanceUp;
 
         transform.LookAt(playerCamera.transform);
         transform.Rotate(0, 180, 0);
