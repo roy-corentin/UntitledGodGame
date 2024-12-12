3f6e6f326d35ec6535252ddc5a0ae41117ecb4b5
Arthur Decaen <arthur.decaen@epitech.eu>
2024-12-12 21:36:56 +0100

    FEAT: add light change in day/night cycle

--- Assets/Scripts/Behaviours/DayNightCycle.cs
+++ Assets/Scripts/Behaviours/DayNightCycle.cs
@@ -8,19 +8,29 @@ public enum TimeOfDay
 
 public class DayNightCycle : MonoBehaviour
 {
+    [Header("Time Settings")]
     public float dayLengthinMinutes = 10;
+    public float timeMultiplier = 1;
+
+    [Header("Infos (Read Only)")]
     public float timeOfDay = 0;
     public float timeOFDayOn24 = 0;
-    public float timeMultiplier = 1;
     public int daySinceStart = 0;
     public TimeOfDay currentTimeOfDay = TimeOfDay.Day;
-    private TimeOfDay lastTimeOfDay = TimeOfDay.Day;
+
+    [Header("Skybox")]
     public Material skyboxMaterial;
-    public Light sun;
-    public Camera mainCamera;
     public float lerpSpeed = 1;
     public Color32 dayColor = new(255, 255, 255, 255);
     public Color32 nightColor = new(0, 0, 0, 255);
+
+    [Header("Light")]
+    public Light sun;
+    public float lightLerpSpeed = 1;
+    public Color32 lightDayColor = new(255, 244, 214, 255);
+    public Color32 lightNightColor = new(0, 0, 0, 255);
+
+    private TimeOfDay lastTimeOfDay = TimeOfDay.Day;
     public static DayNightCycle Instance;
 
     public void Awake()
@@ -63,7 +73,10 @@ public class DayNightCycle : MonoBehaviour
     {
         Color32 targetColor = currentTimeOfDay == TimeOfDay.Day ? dayColor : nightColor;
         Color32 currentColor = skyboxMaterial.GetColor("_Tint");
-        Color32 newColor = Color32.Lerp(currentColor, targetColor, Time.deltaTime * lerpSpeed);
+        Color32 newColor = Color32.Lerp(currentColor, targetColor, Time.deltaTime * lerpSpeed * timeMultiplier);
         skyboxMaterial.SetColor("_Tint", newColor);
+
+        Color32 targetLightColor = currentTimeOfDay == TimeOfDay.Day ? lightDayColor : lightNightColor;
+        sun.color = Color32.Lerp(sun.color, targetLightColor, Time.deltaTime * lightLerpSpeed * timeMultiplier);
     }
 }
